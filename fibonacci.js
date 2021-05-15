7e132eb4d8bdbd50dfc23b2eb318be7f5bd252bb
murthyganti <murthysganti@gmail.com>
2021-05-15 13:06:46 -0400

    40th fibonacci number with memoization

--- fibonacci.js
+++ fibonacci.js
@@ -17,8 +17,21 @@ console.log(fib(6));
 console.log(fib(8));
  
 // The above apporach is O(2**n) in time and space, it takes forever for large numbers.
-// So let's use memoization to speed up and make it 0(n) in space and time -https://en.wikipedia.org/wiki/Memoization
+// So let's use Memoization to speed up and make it 0(n) in space and time -https://en.wikipedia.org/wiki/Memoization
+// Memoization -- store function call results.
+// use JS object key will be arg to function, values will be return value.
 
+const fibMemo = (n, memo = {}) => {
+    if(n in memo) return memo[n]; // if n already exisits in memo return the value.
+    if(n <=2 ) return 1; // firs and second fibonacci numbers are 1 & 1.
+    memo[n] = fibMemo(n-1,memo)+fibMemo(n-2,memo);
+    return memo[n];
+};
 
+console.log(fib(1));
+console.log(fib(2));   
+console.log(fib(16)); // 987
+console.log(fib(20));  // 6765
+console.log(fib(40)); // 102334155
 
  
