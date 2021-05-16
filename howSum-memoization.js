f985d1bd2034f3546d658f0cf4d9d2d0051a4c61
murthyganti <murthysganti@gmail.com>
2021-05-16 10:59:06 -0400

    howSum memoization instructions

---
+++ howSum-memoization.js
@@ -0,0 +1,8 @@
+/**
+ * Write a function howSum(targetSum,arrayOfNumbers) that takes in targetSum and array of numbers as argumnetments.
+ * The function should return an array containing any combination of elements that add up to exactly the targetSum.
+ * If there is no combination that adds up to the targetSum, then return null.
+ * If there are multiple combinations -- return any single one.
+ *  
+ *
+ */
