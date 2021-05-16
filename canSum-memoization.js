b0a52a77f562a4f4e3a621977e2a396f3bab6c71
murthyganti <murthysganti@gmail.com>
2021-05-15 21:01:07 -0400

    added memoization solution for canSum

--- canSum-memoization.js
+++ canSum-memoization.js
@@ -9,6 +9,8 @@
 // canSum(7,[3,4,0,7]) => should return true.
 
 // recursive way
+// space complexcity O(m) -- height of tree
+// Time complexcity O(n**m) -- where m is target sum, n is number of elements in array.
 
 const canSum = (targetSum,arrayofNumbers) => {
     //base cases
@@ -26,7 +28,31 @@ const canSum = (targetSum,arrayofNumbers) => {
 // tests
 console.log(canSum(3,[2,1,0,4]));
 console.log(canSum(5,[2,1,0,4]));
-console.log(canSum(7,[2,1,0,4]));
-//console.log(canSum(7,[3,4,0,7]));
-//console.log(canSum(7,[2,1,0]));
+console.log(canSum(7,[2,3]));
+console.log(canSum(7,[5,3,4]));
+console.log(canSum(8,[2,3,5]));
 //console.log(canSum(300,[5,199,12,1,6]));
+
+const canSumMemo = (targetSum,arrayofNumbers, memo = {}) => {
+    // Note that arrayOfNumbers doesn't change only targetSum changes during recursion. so use that as key.
+    if (targetSum in memo) return memo[targetSum];
+    //base cases
+    if(targetSum === 0) return true;
+    if(targetSum < 0) return false;
+    for(let num of arrayofNumbers){
+        const remainder = targetSum - num;
+        if(canSum(remainder,arrayofNumbers, memo) === true){
+            memo[targetSum] = true;
+            return true;
+        }
+    }
+    memo[targetSum] = false;
+    return false;
+}
+
+console.log(canSumMemo(3,[2,1,0,4]));
+console.log(canSumMemo(5,[2,1,0,4]));
+console.log(canSumMemo(7,[2,3]));
+console.log(canSumMemo(7,[5,3,4]));
+console.log(canSumMemo(8,[2,3,5]));
+console.log(canSumMemo(300,[5,199,12,1,6]));
